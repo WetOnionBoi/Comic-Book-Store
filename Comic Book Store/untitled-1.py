86d748be7e79ea0a5ff5548263a43dc28b08e436
WetOnionBoi <37384973+WetOnionBoi@users.noreply.github.com>
2019-03-15 12:39:00 +1300

    Add files via upload

--- Comic Book Store/untitled-1.py
+++ Comic Book Store/untitled-1.py
@@ -10,19 +10,20 @@ class Comic:
     #signifies a private variable. not to be used outside of this class.
     _ids = count (0)
 
-    def __init__(self, comic_name, comic_stock):
+    def __init__(self, comic_name, image, comic_stock):
         #not passing ID as we want it to create it.
         self.id = next(self._ids)
         self.name = comic_name
+        self.image = image
         self.stock = comic_stock
 
 
 
     #Test Data
 comics  = [
-          Comic("Super-Dude", 8),
-          Comic("Lizard-Man",  12),
-          Comic("Water-Woman", 3)
+          Comic("Super-Dude","Superd.jpg", 8),
+          Comic("Lizard-Man","lizard.jpg",  12),
+          Comic("Water-Woman","waterwoman.png", 3)
           ]
 
 #index page
@@ -32,10 +33,10 @@ def index():
     #need this function to attach the decorators above.
     pass
 
-@route('/comics')
-@view('comics')
-def comics():
-    data = dict (comic_list = Comic)
+@route('/sell_comics')
+@view('sell_comics')
+def sell_comics():
+    data = dict (comics_list=comics)
     return data
 
 
